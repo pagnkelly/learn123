53ca9f766d925426add799694a0103778f17ab88
pagnkelly <632228198@qq.com>
2022-07-02 15:21:33 +0800

    feat: element plus

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## 简介
 
 [live](https://learn123.vercel.app/)
+
+[牦牛剃须中](https://americanexpress.io/yak-shaving/)
--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'learn123',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: '前端学习门户网站' },
     {
       name: 'theme-color',
       content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
